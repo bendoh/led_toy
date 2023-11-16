1f534558d89c1b31932a9923b900dedd9e9a086b
Benjamin Doherty <bendohmv@gmail.com>
2023-11-16 11:19:51 -0500

    Fix various issues and refactor code

--- main/main.c
+++ main/main.c
@@ -15,7 +15,7 @@
 #include "esp_err.h"
 #include "driver/gpio.h"
 
-// GPIO assignment
+// Data pin for programming the strip
 #define LED_STRIP_GPIO  4
 // Numbers of the LED in the strip
 #define NUM_LEDS 60
@@ -57,15 +57,15 @@ led_strip_handle_t configure_led(void)
 
 const uint32_t VALUE_SPEED_GPIO = GPIO_NUM_41;
 const uint32_t BRIGHTNESS_CHANGE_GPIO = GPIO_NUM_42;
-const uint32_t HUE_SPEED_GPIO = GPIO_NUM_2;
+const uint32_t HUE_SPEED_GPIO = GPIO_NUM_40;
 
-double valuespeed = 0.5;
+int32_t valuespeed = 0;
 bool valuedir = 1;
 
-uint8_t brightness = 0x87;
+int32_t brightness = 0x87;
 bool brightnessdir = 1;
 
-double huespeed = 0.5;
+int32_t huespeed = 0;
 bool huedir = 1;
 
 static QueueHandle_t gpio_evt_queue = NULL;
@@ -77,6 +77,24 @@ static void IRAM_ATTR gpio_isr_handler(void* arg)
     xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
 }
 
+static void change_speed(int32_t *speed, bool *dir, int32_t low, int32_t high, uint32_t step) {
+    if(*dir == 1) {
+        *speed += step;
+
+        if (*speed > high) {
+            *speed = high;
+            *dir = 0;
+        }
+    } else {
+        *speed -= step;
+        
+        if (*speed <= low) {
+            *speed = low;
+            *dir = 1;
+        }
+    } 
+}
+
 static void gpio_task(void* arg)
 {
     uint32_t io_num;
@@ -84,108 +102,56 @@ static void gpio_task(void* arg)
         if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
             if(gpio_get_level(io_num) == 1) {
                 ESP_LOGI(TAG, "Button on %lu pressed!", io_num);
+
                 if (io_num == VALUE_SPEED_GPIO) {
-                    if(valuedir == 1) {
-                        valuespeed += 0.5;
-
-                        if (valuespeed > 5) {
-                            valuespeed = 5;
-                            valuedir = 0;
-                        }
-                    } else {
-                        valuespeed -= 0.5;
-                        
-                        if (valuespeed <= 0) {
-                            valuespeed = 0;
-                            valuedir = 1;
-                        }
-                    } 
-
-                    ESP_LOGI(TAG, "Set valuespeed to %f", valuespeed);
+                    change_speed(&valuespeed, &valuedir, -8, 8, 1);
+                    ESP_LOGI(TAG, "Set valuespeed to %d", (int16_t) valuespeed);
                 }
                 else if (io_num == HUE_SPEED_GPIO) {
-                    if(huedir == 1) {
-                        huespeed += 0.5;
-
-                        if (huespeed > 5) {
-                            huespeed = 5;
-                            huedir = 0;
-                        }
-                    } else {
-                        huespeed -= 0.5;
-                        
-                        if (huespeed <= 0) {
-                            huespeed = 0;
-                            huedir = 1;
-                        }
-                    } 
-
-                    ESP_LOGI(TAG, "Set huespeed to %f", huespeed);
+                    change_speed(&huespeed, &huedir, -8, 8, 1);
+                    ESP_LOGI(TAG, "Set huespeed to %d", (int16_t) huespeed);
                 }
                 else if (io_num == BRIGHTNESS_CHANGE_GPIO) {
-                    if(brightness == 0xff) {
-                        brightnessdir = 0;
-                    } else if (brightness == 0x00) {
-                        brightnessdir = 1;
-                    } 
-
-                    if(brightnessdir) {
-                        brightness += 0x0f;
-                    } else {
-                        brightness -= 0x0f;
-                    }
-
-                    ESP_LOGI(TAG, "Set brightness to 0x%x", brightness);
+                    change_speed(&brightness, &brightnessdir, 0, 0xff, 0x0f);
+                    ESP_LOGI(TAG, "Set brightness to 0x%x", (uint8_t) brightness);
                 }
             }
         }
     }
 }
 
-void app_main(void)
-{
+void configure_pin(uint32_t pin) {
     gpio_config_t value_speed_button_io_conf = {
         .intr_type = GPIO_INTR_POSEDGE,
         .mode = GPIO_MODE_DEF_INPUT,
         .pull_down_en = 1,
-        .pin_bit_mask = 1ULL << VALUE_SPEED_GPIO
+        .pin_bit_mask = 1ULL << pin
     };
+
     gpio_config(&value_speed_button_io_conf);
 
-    gpio_config_t brightness_button_io_conf = {
-        .intr_type = GPIO_INTR_POSEDGE,
-        .mode = GPIO_MODE_DEF_INPUT,
-        .pull_down_en = 1,
-        .pin_bit_mask = 1ULL << BRIGHTNESS_CHANGE_GPIO
-    };
-    gpio_config(&brightness_button_io_conf);
+    gpio_isr_handler_add(pin, gpio_isr_handler, (void*) pin);
+}
 
-    gpio_config_t hue_speed_button_io_conf = {
-        .intr_type = GPIO_INTR_POSEDGE,
-        .mode = GPIO_MODE_DEF_INPUT,
-        .pull_down_en = 1,
-        .pin_bit_mask = 1ULL << HUE_SPEED_GPIO
-    };
-    gpio_config(&hue_speed_button_io_conf);
-
+void app_main(void)
+{
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
     xTaskCreate(gpio_task, "gpio_task", 2048, NULL, 10, NULL);
 
     //install gpio isr service
     gpio_install_isr_service(0);
 
-    //hook isr handler for specific gpio pin
-    gpio_isr_handler_add(VALUE_SPEED_GPIO, gpio_isr_handler, (void*) VALUE_SPEED_GPIO);
-    gpio_isr_handler_add(BRIGHTNESS_CHANGE_GPIO, gpio_isr_handler, (void*) BRIGHTNESS_CHANGE_GPIO);
-    gpio_isr_handler_add(HUE_SPEED_GPIO, gpio_isr_handler, (void*) HUE_SPEED_GPIO);
+    configure_pin(VALUE_SPEED_GPIO);
+    configure_pin(BRIGHTNESS_CHANGE_GPIO);
+    configure_pin(HUE_SPEED_GPIO);
 
     led_strip_handle_t led_strip = configure_led();
 
     uint8_t *hues = calloc(NUM_LEDS, sizeof(uint8_t));
     double *values = calloc(NUM_LEDS, sizeof(double));
 
-    int huerotate = 0;
-    int valuerotate = 0;
+    int64_t huerotate = 0;
+    int64_t valuerotate = 0;
 
     // Rainbow length = number of colors in a rainbow
     // Initialize the colors w a rainbow using HSV
@@ -198,8 +164,9 @@ void app_main(void)
         ESP_LOGI(TAG, "Pixel %lu got hue %d with value %f", i, hues[i], values[i]);
     }
 
+    ESP_LOGI(TAG, "What?");
     while (1) {
-        // Set each hue to the value of hues[i]
+        // Set each pixel to the HSV value
         for (int i = 0; i < NUM_LEDS; i++) {
             uint8_t hue = hues[(i + huerotate) % NUM_LEDS];
             uint8_t sat = 0xFF;
